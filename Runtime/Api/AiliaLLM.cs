afbf12d6a6665a937875142d81e0e306b775c254
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-10-03 19:46:37 +0900

    Update API

--- Runtime/Api/AiliaLLM.cs
+++ Runtime/Api/AiliaLLM.cs
@@ -10,7 +10,6 @@ using System.Runtime.InteropServices;
 namespace ailiaLLM{
 public class AiliaLLM
 {
-
     /* Native Binary 定義 */
 
     #if (UNITY_IPHONE && !UNITY_EDITOR) || (UNITY_WEBGL && !UNITY_EDITOR)
@@ -23,15 +22,199 @@ public class AiliaLLM
         #endif
     #endif
 
+    /****************************************************************
+    * ライブラリ状態定義
+    **/
+
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_SUCCESS
+    * @brief 成功
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_SUCCESS
+    * @brief Successful
+    */
+    public const int AILIA_LLM_STATUS_SUCCESS = (0);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_INVALID_ARGUMENT
+    * @brief 引数が不正
+    * @remark API呼び出し時の引数を確認してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_INVALID_ARGUMENT
+    * @brief Incorrect argument
+    * @remark Please check argument of called API.
+    */
+    public const int AILIA_LLM_STATUS_INVALID_ARGUMENT = (-1);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_ERROR_FILE_API
+    * @brief ファイルアクセスに失敗した
+    * @remark 指定したパスのファイルが存在するか、権限を確認してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_ERROR_FILE_API
+    * @brief File access failed.
+    * @remark Please check file is exist or not, and check access permission.
+    */
+    public const int AILIA_LLM_STATUS_ERROR_FILE_API = (-2);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_INVALID_VERSION
+    * @brief 構造体バージョンが不正
+    * @remark API呼び出し時に指定した構造体バージョンを確認し、正しい構造体バージョンを指定してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_INVALID_VERSION
+    * @brief Incorrect struct version
+    * @remark Please check struct version that passed with API and please pass correct struct version.
+    */
+    public const int AILIA_LLM_STATUS_INVALID_VERSION = (-3);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_BROKEN
+    * @brief 壊れたファイルが渡された
+    * @remark モデルファイルが破損していないかを確認し、正常なモデルを渡してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_BROKEN
+    * @brief A corrupt file was passed.
+    * @remark Please check model file are correct or not, and please pass correct model.
+    */
+    public const int AILIA_LLM_STATUS_BROKEN = (-4);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_MEMORY_INSUFFICIENT
+    * @brief メモリが不足している
+    * @remark メインメモリやVRAMの空き容量を確保してからAPIを呼び出してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_MEMORY_INSUFFICIENT
+    * @brief Insufficient memory
+    * @remark Please check usage of main memory and VRAM. And please call API after free memory.
+    */
+    public const int AILIA_LLM_STATUS_MEMORY_INSUFFICIENT = (-5);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_THREAD_ERROR
+    * @brief スレッドの作成に失敗した
+    * @remark スレッド数などシステムリソースを確認し、リソースを開放してからAPIを呼び出してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_THREAD_ERROR
+    * @brief Thread creation failed.
+    * @remark Please check usage of system resource = (e.g. thread). And please call API after release system  resources.
+    */
+    public const int AILIA_LLM_STATUS_THREAD_ERROR = (-6);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_INVALID_STATE
+    * @brief 内部状態が不正
+    * @remark APIドキュメントを確認し、呼び出し手順が正しいかどうかを確認してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_INVALID_STATE
+    * @brief The internal status is incorrect.
+    * @remark Please check API document and API call steps.
+    */
+    public const int AILIA_LLM_STATUS_INVALID_STATE = (-7);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_CONTEXT_FULL
+    * @brief コンテキスト長を超えました
+    * @remark SetPromptに与えるコンテキスト長を削減してください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_CONTEXT_FULL
+    * @brief Exceeded the context length.
+    * @remark Please reduce the context length given to SetPrompt.
+    */
+    public const int AILIA_LLM_STATUS_CONTEXT_FULL = (-8);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_UNIMPLEMENTED
+    * @brief 未実装
+    * @remark
+    * 指定した環境では未実装な機能が呼び出されました。エラー内容をドキュメント記載のサポート窓口までお問い合わせください。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_UNIMPLEMENTED
+    * @brief Unimplemented error
+    * @remark The called API are not available on current environment. Please contact support desk that described on
+    * document.
+    */
+    public const int AILIA_LLM_STATUS_UNIMPLEMENTED = (-15);
+    /**
+    * \~japanese
+    * @def AILIA_LLM_STATUS_OTHER_ERROR
+    * @brief 不明なエラー
+    * @remark その他のエラーが発生しました。
+    *
+    * \~english
+    * @def AILIA_LLM_STATUS_OTHER_ERROR
+    * @brief Unknown error
+    * @remark The misc error has been occurred.
+    */
+    public const int AILIA_LLM_STATUS_OTHER_ERROR = (-128);
+
+    /****************************************************************
+    * チャットメッセージ
+    **/
     [StructLayout(LayoutKind.Sequential)]
     public class AILIAChatMessage {
+        /**
+        * @brief Represent the role. (system, user, assistant)
+        */
         public IntPtr role;
+        /**
+        * @brief Represent the content of the message.
+        */
         public IntPtr content;
     }
 
+    /**
+    * \~japanese
+    * @brief LLMオブジェクトを作成します。
+    * @param llm LLMオブジェクトポインタへのポインタ
+    * @param n_ctx コンテキスト長（0でモデルのデフォルト）
+    * @return
+    *   成功した場合は \ref AILIA_LLM_STATUS_SUCCESS 、そうでなければエラーコードを返す。
+    * @details
+    *   LLMオブジェクトを作成します。
+    *
+    * \~english
+    * @brief Creates a LLM instance.
+    * @param llm A pointer to the LLM instance pointer
+    * @param n_ctx Context length for model (0 is model default）
+    * @return
+    *   If this function is successful, it returns  \ref AILIA_LLM_STATUS_SUCCESS , or an error code otherwise.
+    * @details
+    *   Creates a LLM instance.
+    */
     [DllImport(LIBRARY_NAME)]
-    public static extern int ailiaLLMCreate(ref IntPtr net, int n_ctx);
+    public static extern int ailiaLLMCreate(ref IntPtr net, uint n_ctx);
 
+    /**
+    * \~japanese
+    * @brief モデルファイルを読み込みます。
+    * @param llm LLMオブジェクトポインタへのポインタ
+    * @param path GGUFファイルのパス
+    * @return
+    *   成功した場合は \ref AILIA_STATUS_SUCCESS 、そうでなければエラーコードを返す。
+    * @details
+    *   GGUFのモデルファイルを読み込みます。
+    *
+    * \~english
+    * @brief Open model file.
+    * @param llm A pointer to the LLM instance pointer
+    * @param path Path for GGUF
+    * @return
+    *   If this function is successful, it returns  \ref AILIA_STATUS_SUCCESS , or an error code otherwise.
+    * @details
+    *   Open a model file for GGUF.
+    */
     #if (UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN)
         [DllImport(LIBRARY_NAME, EntryPoint = "ailiaLLMOpenModelFileW", CharSet=CharSet.Unicode)]
         public static extern int ailiaLLMOpenModelFile(IntPtr net, string path);
@@ -40,18 +223,109 @@ public class AiliaLLM
         public static extern int ailiaLLMOpenModelFile(IntPtr net, string path);
     #endif
 
+    /**
+    * \~japanese
+    * @brief プロンプトを設定します。
+    * @param llm LLMオブジェクトポインタへのポインタ
+    * @param message メッセージの配列
+    * @param message_cnt メッセージの数
+    * @return
+    *   成功した場合は \ref AILIA_STATUS_SUCCESS 、そうでなければエラーコードを返す。
+    * @details
+    *   LLMに問い合わせるプロンプトを設定します。
+    *   ChatHistoryもmessageに含めてください。
+    *
+    * \~english
+    * @brief Set the prompt.
+    * @param llm A pointer to the LLM instance pointer
+    * @param message Array of messages
+    * @param message_cnt Number of messages
+    * @return
+    *   If this function is successful, it returns  \ref AILIA_STATUS_SUCCESS , or an error code otherwise.
+    * @details
+    *   Set the prompt to query the LLM.
+    *   Please include ChatHistory in the message as well.
+    */
     [DllImport(LIBRARY_NAME)]
     public static extern int ailiaLLMSetPrompt(IntPtr net, IntPtr messages, uint messages_len);
 
+    /**
+    * \~japanese
+    * @brief 生成を行います。
+    * @param llm LLMオブジェクトポインタ
+    * @param done 生成が完了したか
+    * @return
+    *   成功した場合は \ref AILIA_LLM_STATUS_SUCCESS 、そうでなければエラーコードを返す。
+    * @details
+    *   デコードした結果はailiaLLMGetDeltaText APIで取得します。
+    *   ailiaLLMGenerateを呼び出すたびに1トークンずつデコードします。
+    *   doneは0か1を取ります。doneが1の場合、生成完了となります。
+    *
+    * \~english
+    * @brief Perform generate
+    * @param llm A LLM instance pointer
+    * @param done Generation complete?
+    * @return
+    *   If this function is successful, it returns  \ref AILIA_LLM_STATUS_SUCCESS , or an error code otherwise.
+    * @details
+    *   The decoded result is obtained through the ailiaLLMGetDeltaText API.
+    *   Each call to ailiaLLMGenerate decodes one token at a time.
+    *   The value of done is 0 or 1. If done is 1, the generation is complete.
+    */
     [DllImport(LIBRARY_NAME)]
-    public static extern int ailiaLLMGenerate(IntPtr net, ref bool done);
+    public static extern int ailiaLLMGenerate(IntPtr net, ref uint done);
 
+    /**
+    * \~japanese
+    * @brief テキストの長さを取得します。(NULL文字含む)
+    * @param llm   LLMオブジェクトポインタ
+    * @param len  テキストの長さ
+    * @return
+    *   成功した場合は \ref AILIA_STATUS_SUCCESS 、そうでなければエラーコードを返す。
+    *
+    * \~english
+    * @brief Gets the size of text. (Include null)
+    * @param llm   A LLM instance pointer
+    * @param len  The length of text
+    * @return
+    *   If this function is successful, it returns  \ref AILIA_STATUS_SUCCESS , or an error code otherwise.
+    */
     [DllImport(LIBRARY_NAME)]
     public static extern int ailiaLLMGetDeltaTextSize(IntPtr net, ref uint len);
 
+    /**
+    * \~japanese
+    * @brief テキストを取得します。
+    * @param llm   LLMオブジェクトポインタ
+    * @param text  テキスト(UTF8)
+    * @param len バッファサイズ
+    * @return
+    *   成功した場合は \ref AILIA_STATUS_SUCCESS 、そうでなければエラーコードを返す。
+    * @details
+    *   ailiaLLMGenerate() を一度も実行していない場合は \ref AILIA_STATUS_INVALID_STATE が返ります。
+    *
+    * \~english
+    * @brief Gets the decoded text.
+    * @param llm   A LLM instance pointer
+    * @param text  Text(UTF8)
+    * @param len  Buffer size
+    * @return
+    *   If this function is successful, it returns  \ref AILIA_STATUS_SUCCESS , or an error code otherwise.
+    * @details
+    *   If  ailiaLLMGenerate()  is not run at all, the function returns  \ref AILIA_STATUS_INVALID_STATE .
+    */
     [DllImport(LIBRARY_NAME)]
     public static extern int ailiaLLMGetDeltaText(IntPtr net, IntPtr text, uint len);
 
+    /**
+    * \~japanese
+    * @brief LLMオブジェクトを破棄します。
+    * @param llm LLMオブジェクトポインタ
+    *
+    * \~english
+    * @brief It destroys the LLM instance.
+    * @param llm A LLM instance pointer
+    */
     [DllImport(LIBRARY_NAME)]
     public static extern void ailiaLLMDestroy(IntPtr net);
 }
--- Runtime/Models/AiliaLLMModel.cs
+++ Runtime/Models/AiliaLLMModel.cs
@@ -19,6 +19,7 @@ public class AiliaLLMModel : IDisposable
 {
 	// instance
 	IntPtr net = IntPtr.Zero;
+	bool logging = true;
 
 	/****************************************************************
 	 * モデル
@@ -27,21 +28,27 @@ public class AiliaLLMModel : IDisposable
 	/**
 	* \~japanese
 	* @brief インスタンスを作成します。
+	* @param n_ctx コンテキスト長（0でモデルのデフォルト）
 	* @return
 	*   成功した場合はtrue、失敗した場合はfalseを返す。
 	*   
 	* \~english
 	* @brief   Create a instance.
+    * @param n_ctx Context length for model (0 is model default）
 	* @return
 	*   If this function is successful, it returns  true  , or  false  otherwise.
 	*/
-	public bool Create(int n_ctx = 0){
+	public bool Create(uint n_ctx = 0){
 		if (net != IntPtr.Zero){
 			Close();
 		}
 
 		int status = AiliaLLM.ailiaLLMCreate(ref net, n_ctx);
 		if (status != 0){
+			if (logging)
+			{
+				Debug.Log("ailiaLLMCreate failed " + status);
+			}
 			return false;
 		}
 
@@ -70,6 +77,10 @@ public class AiliaLLMModel : IDisposable
 		
 		status = AiliaLLM.ailiaLLMOpenModelFile(net, model_path);
 		if (status != 0){
+			if (logging)
+			{
+				Debug.Log("ailiaLLMOpenModelFile failed " + status);
+			}
 			return false;
 		}
 
@@ -79,6 +90,7 @@ public class AiliaLLMModel : IDisposable
 	/****************************************************************
 	 * 開放する
 	 */
+
 	/**
 	* \~japanese
 	* @brief インスタンスを破棄します。
@@ -123,9 +135,22 @@ public class AiliaLLMModel : IDisposable
 	}
 
 	/****************************************************************
-	 * エンコードとデコード
+	 * チャット
 	 */
 
+	/**
+	* \~japanese
+	* @brief プロンプトを設定します。
+	* @param messages          プロンプトメッセージ。
+	* @return
+	*   成功した場合はtrue、失敗した場合はfalseを返す。
+	*   
+	* \~english
+	* @brief   Set prompt messages.
+	* @param messages          Prompt messages
+	* @return
+	*   If this function is successful, it returns  true  , or  false  otherwise.
+	*/
 	public bool SetPrompt(List<AiliaLLMChatMessage> messages)
 	{
 		List<GCHandle> handle_list = new List<GCHandle>();
@@ -152,26 +177,24 @@ public class AiliaLLMModel : IDisposable
 			handle_list.Add(content_handle);
 		}
 
-        int size = Marshal.SizeOf(typeof(AiliaLLM.AILIAChatMessage)) * message_list.Length;
-        IntPtr ptr = Marshal.AllocHGlobal(size);
+		int size = Marshal.SizeOf(typeof(AiliaLLM.AILIAChatMessage)) * message_list.Length;
+		IntPtr ptr = Marshal.AllocHGlobal(size);
 
 		int status = 0;
 
-        try
-        {
-            for (int i = 0; i < message_list.Length; i++)
-            {
-                IntPtr offset = new IntPtr(ptr.ToInt64() + i * Marshal.SizeOf(typeof(AiliaLLM.AILIAChatMessage)));
-                Marshal.StructureToPtr(message_list[i], offset, false);
-            }
+		try
+		{
+			for (int i = 0; i < message_list.Length; i++)
+			{
+				IntPtr offset = new IntPtr(ptr.ToInt64() + i * Marshal.SizeOf(typeof(AiliaLLM.AILIAChatMessage)));
+				Marshal.StructureToPtr(message_list[i], offset, false);
+			}
 
 			status = AiliaLLM.ailiaLLMSetPrompt(net, ptr, (uint)len);
-
-            //ProcessStructArray(ptr, message_list.Length);
-        }
-        finally
-        {
-            Marshal.FreeHGlobal(ptr);
+		}
+		finally
+		{
+			Marshal.FreeHGlobal(ptr);
 		}
 
 		for (int i = 0; i < handle_list.Count; i++){
@@ -179,17 +202,36 @@ public class AiliaLLMModel : IDisposable
 		}
 
 		if (status != 0){
+			if (logging)
+			{
+				Debug.Log("ailiaLLMSetPrompt failed " + status);
+			}
 			return false;
 		}
 
 		return true;
 	}
 
+	/**
+	* \~japanese
+	* @brief 生成結果のテキストを取得します。
+	* @return
+	*   テキストを返します。
+	*   
+	* \~english
+	* @brief   Set prompt messages.
+	* @return
+	*   It returns  text.
+	*/
 	public string GetDeltaText()
 	{
 		uint len = 0;
 		int status = AiliaLLM.ailiaLLMGetDeltaTextSize(net, ref len);
 		if (status != 0){
+			if (logging)
+			{
+				Debug.Log("ailiaLLMGetDeltaTextSize failed " + status);
+			}
 			return "";
 		}
 		byte[] text = new byte [len];
@@ -198,6 +240,10 @@ public class AiliaLLMModel : IDisposable
 		status = AiliaLLM.ailiaLLMGetDeltaText(net, output, len);
 		handle.Free();
 		if (status != 0){
+			if (logging)
+			{
+				Debug.Log("ailiaLLMGetDeltaText failed " + status);
+			}
 			return "";
 		}
 		byte[] text_split = new byte [len - 1]; // NULLも時の削除
@@ -222,8 +268,14 @@ public class AiliaLLMModel : IDisposable
 	*/
 	public bool Generate(ref bool done)
 	{
-		int status = AiliaLLM.ailiaLLMGenerate(net, ref done);
+		uint done_uint = 0;
+		int status = AiliaLLM.ailiaLLMGenerate(net, ref done_uint);
+		done = (done_uint == 1);
 		if (status != 0){
+			if (logging)
+			{
+				Debug.Log("ailiaLLMGenerate failed " + status);
+			}
 			return false;
 		}
 		return true;
